30c9a7a0dd3de224cb4d37c16953efc95fef48e2
Miika Petäjäniemi <miika.petajaniemi@holda.fi>
2021-08-04 21:55:33 +0300

    update

---
+++ install-wm.sh
@@ -0,0 +1,17 @@
+PKGS="gvim \
+dunst \
+rofi \
+picom \
+polybar \
+pamixer \
+i3lock \
+pa-applet \
+pavucontrol \
+nitrogen \
+playerctl \
+kitty \
+peek \
+gnome-screenshot \
+i3exit"
+
+sudo pacman -S $PKGS
