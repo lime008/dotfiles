0de81a97603e77be62d14aa5206986720b713e8a
lime008 <miika.petajaniemi@holda.fi>
2021-10-01 16:00:08 +0300

    update polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,5 +3,6 @@ killall -q polybar
 echo "---" | tee -a /tmp/polybar_main.log
 polybar main 2>&1 | tee -a /tmp/polybar_main.log & disown
 polybar second 2>&1 | tee -a /tmp/polybar_second.log & disown
+polybar laptop 2>&1 | tee -a /tmp/polybar_second.log & disown
 
 echo "Bars launched..."
