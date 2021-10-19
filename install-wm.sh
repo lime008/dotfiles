35b61c20e5b9e13d895a526a5f19a1e2e2b8936c
Miika Petäjäniemi <miika.petajaniemi@solita.fi>
2021-10-19 11:44:54 +0300

    add ripgrep

--- install-wm.sh
+++ install-wm.sh
@@ -14,6 +14,8 @@ peek \
 maim \
 viu \
 bat \
+autorandr \
+ripgrep \
 i3exit"
 
 sudo pacman -S $PKGS
@@ -21,3 +23,6 @@ sudo pacman -S $PKGS
 # Install vim plug for neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+sudo systemctl enable autorandr
+sudo systemctl start autorandr
