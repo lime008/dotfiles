b77a08166787363b0fbc0a8e48a32e514b3058ab
Miika Petäjäniemi <miika.petajaniemi@opsert.fi>
2023-03-14 10:08:01 +0200

    Update configuration as open_on_setup optinon will be deprecated

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -39,7 +39,6 @@ local list = {
 require("nvim-tree").setup({
 	disable_netrw = true,
 	hijack_netrw = true,
-	open_on_setup = false,
 	ignore_ft_on_setup = {},
 	open_on_tab = false,
 	hijack_cursor = false,
@@ -80,3 +79,25 @@ require("nvim-tree").setup({
 		},
 	},
 })
+
+local function open_nvim_tree(data)
+	-- buffer is a [No Name]
+	local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+	-- buffer is a directory
+	local directory = vim.fn.isdirectory(data.file) == 1
+
+	if not no_name and not directory then
+		return
+	end
+
+	-- change to the directory
+	if directory then
+		vim.cmd.cd(data.file)
+	end
+
+	-- open the tree
+	require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
