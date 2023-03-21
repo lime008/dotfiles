61da73520fb692b0c41e8a084818d519a669cec2
Miika Petäjäniemi <miika.petajaniemi@opsert.fi>
2023-03-21 18:46:25 +0200

    Disable markdown fence highlights

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,19 +1,4 @@
 vim.opt.background = "dark"
 vim.cmd("colorscheme limetty") -- base16-snazzy base16-porple base16-gruvbox-dark-hard just if I change my mind
 
--- markdown highlighting for code fences
-vim.g.markdown_fenced_languages = {
-	"bash=sh",
-	"javascript",
-	"js=javascript",
-	"json=javascript",
-	"typescript",
-	"ts=typescript",
-	"php",
-	"html",
-	"css",
-	"rust",
-	"go",
-}
-
 require("colorizer").setup()
