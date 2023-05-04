30205b31296b404aea7eae5f5b3f35443eb72504
Miika Petäjäniemi <miika.petajaniemi@opsert.fi>
2023-05-04 12:40:53 +0300

    Autoinstall packer on start

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
 	-- [NAVIGATION]
@@ -91,4 +104,7 @@ return require("packer").startup(function(use)
 
 	-- TinyGo
 	use("sago35/tinygo.vim")
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
